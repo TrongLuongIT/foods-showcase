7f790bbcfe340c706124d671fb8f1ac13b9cc579
TrongLuongIT <luonghoangtrongit@gmail.com>
2026-04-10 22:03:02 +0700

    test pagespeed

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -20,7 +20,7 @@ export default function GoogleMap() {
       </div>
       <div className="col-12 col-md-6">
         <div className="text-center ratio ratio-1x1">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d812.9976264987912!2d106.67412225666409!3d10.771144991658767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ff067f8673d%3A0x1cd441dc210a95a0!2sKokoria!5e0!3m2!1svi!2s!4v1774283043753!5m2!1svi!2s" style={{border: 0}} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+          {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d812.9976264987912!2d106.67412225666409!3d10.771144991658767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ff067f8673d%3A0x1cd441dc210a95a0!2sKokoria!5e0!3m2!1svi!2s!4v1774283043753!5m2!1svi!2s" style={{border: 0}} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe> */}
         </div>
       </div>
     </div>
--- src/components/common/ImageMarquee.tsx
+++ src/components/common/ImageMarquee.tsx
@@ -37,7 +37,6 @@ export default function ImageMarquee({ images, speed = 30 }: ImageMarqueeProps) 
               alt={`CCTV-${index}`}
               fill
               className="object-fit-cover"
-              sizes="(max-width: 768px) 80vw, (max-width: 1200px) 50vw, 300px"
             />
           </div>
         ))}
